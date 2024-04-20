fc547929c04ffa93f54401fdcfd6b3146ee36f52
blato-laptop <blator02@gmail.com>
2024-04-20 16:03:13 +0200

    bash ex3 done

--- lab_8_bash/ex3.sh
+++ lab_8_bash/ex3.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-echo "liczba argumentow:" "$#"
+option=""
+# echo "liczba argumentow:" "$#"
 if [[ $# -eq 0 ]]; then
-	echo "nie podano slowa"
-	read -p "podaj slowo: " arg
-	echo "twoje slowo:" "$arg"
+	option="--unsolved"
 elif [[ $# -eq 1 ]]; then
-	arg=$1
-	echo "twoje slowo:" "$arg"	
+	if [ $1 != "--solved" ] && [ $1 != "-S" ]; then
+        echo "nieprawidlowy argument"
+	    exit
+    fi	
+    option="--solved" # lub $1
 else
 	echo "nieprawidlowa liczba argumentow"
 	exit
@@ -20,49 +22,44 @@ fi
 
 file="lab_07_words_with_defs.txt"
 
-# wcount=$(wc -l < $file) # missing one?!?!
-# ((wcount++))
-
-# plik jest posortowany alfabetycznie
-function random_word() {
+# plik jest posortowany alfabetycznie - ale bez różnicy
+random_word() {
     # randomline=$((1 + $RANDOM % $wcount))
     # # randomword=$(awk 'NR==5' $file)
     # randomdef=$(sed -n "${randomline}p" $file) 
     # # double quotes to accept variables!!!
     # # and brackets also so that I can append p later
 
-    # randomword=$(awk '{print $1}' <<< "$randomdef")
-    # ccount=$(echo -n "$randomword" | wc -m)
-    # ccount=${#randomword}
-    randomdef=$(grep -i "^$1" "$file" | shuf -n 1)
+    local randomline=$(grep -i "^$1" "$file" | shuf -n 1)
     # grep -i --> ignore case
     # shuf -n 1 --> random permutation of at most 1 line --> 1 random line
-    randomword=$(awk '{print $1}' <<< "$randomdef")
+    local randomword=$(awk '{print $1}' <<< "$randomline")
+    local randomdef=$(awk '{$1=""; print ">" $0}' <<< "$randomline")
     # co to jest to <<< ???
-    echo "$randomword"
+    echo "$randomword" "$randomdef" # nic nie dają chyba te cudzysłowy jednak
 }
 
-# echo "$wcount"
-# echo "$randomline"
-# echo "$randomdef"
-rw=$(random_word .) # any letter
-# echo "$randomword"
-# echo "${#randomword}"
+# solution=$(random_word .) # any letter
+# https://gist.github.com/jimratliff/d735a57eef05b650d4a17f10b7da64d9
+ read -r solution IGNORE <<< "$(random_word .)" # jakim cudem to działa?
 
-# for ((i = 0; i < ${#rw}; ++i)); do
-#     char="${rw:i:1}"
-#     extrarw=$(random_word $char)
-#     echo "|${extrarw:0:1}|${extrarw:1:${#extrarw}-1}"
-# done
+clue_defs=()
 
-for ((i = 0; i < ${#rw}; ++i)); do
-    char="${rw:i:1}"
-    extrarw=$(random_word $char)
-    echo "|${extrarw:0:1}|${extrarw:1:${#extrarw}-1}"
-    transformed_word=$(echo "$extrarw" | sed 's/./_|/g')
-    echo "|$transformed_word"
+for ((i = 0; i < ${#solution}; ++i)); do
+    char="${solution:i:1}"
+
+    read -r clue clue_def <<< "$(random_word $char)"
+    clue_defs+=("$clue_def")
+
+    if [ "$option" == "--solved" ]; then #|| [ "$option" == "-S" ]; then
+        echo "|${clue:0:1}|${clue:1:${#clue}-1}"
+    else
+        clue_hidden=$(echo "$clue" | sed 's/./_|/g')
+        echo "|$clue_hidden"
+    fi
+
 done
 
-# dodać punkt 4.
+printf '%s\n' "${clue_defs[@]}"
+
 # można dodać że to dodatkowe słowo nie może być takie samo jak główne słowo
-# no i to musi podawać definicje poniżej żeby miało sens XD
